c9bfb233987ac148c41965e231a9500a5b3bd0ae
Victor Higaki <victorhigaki@outlook.com>
2022-12-14 13:49:49 -0300

    page route with parameter and geting a parameter from a route

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Job Details Page</h1>
+  <!-- <p>THe job id is {{ $route.params.id }}</p> -->
+  <p>THe job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        id: this.$route.params.id
+    };
+  },
+};
+</script>
+
+<style>
+</style>
