be51833f62e5459c73c8b2e654304cd02942fcf2
Victor Higaki <victorhigaki@outlook.com>
2022-12-14 16:33:32 -0300

    params to router-link and props

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,18 +1,20 @@
 <template>
   <h1>Job Details Page</h1>
   <!-- <p>THe job id is {{ $route.params.id }}</p> -->
-  <p>THe job id is {{ id }}</p>
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-        id: this.$route.params.id
-    };
-  },
+  props: ["id"],
+  // data() {
+  //   return {
+  //       id: this.$route.params.id
+  //   };
+  // },
 };
 </script>
 
 <style>
+
 </style>
