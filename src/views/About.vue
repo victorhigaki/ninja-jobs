7f9c37808493993f1e25d847bee5ad1fd1c87fea
Victor Higaki <victorhigaki@outlook.com>
2022-12-14 01:01:24 -0300

    project setup

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deserunt
+      blanditiis velit assumenda, ea suscipit natus nesciunt earum eos veniam ab
+      unde est, pariatur quasi nihil impedit ad! Temporibus dolores beatae, vero
+      neque provident praesentium deleniti necessitatibus. Fugiat, ipsa dolores
+      corporis voluptatum quam consequuntur assumenda recusandae quod minima
+      alias ab voluptatibus unde eligendi numquam vitae dolore placeat ipsum
+      saepe ipsam ratione nulla. Laborum ratione quis odit exercitationem et ex
+      voluptatum expedita vero magni assumenda. Quidem rem quia ex ducimus
+      delectus ipsum soluta eveniet quae veritatis? Vero iusto totam facere,
+      autem molestiae facilis adipisci, illum voluptatibus nulla officiis
+      architecto qui repellat!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deserunt
+      blanditiis velit assumenda, ea suscipit natus nesciunt earum eos veniam ab
+      unde est, pariatur quasi nihil impedit ad! Temporibus dolores beatae, vero
+      neque provident praesentium deleniti necessitatibus. Fugiat, ipsa dolores
+      corporis voluptatum quam consequuntur assumenda recusandae quod minima
+      alias ab voluptatibus unde eligendi numquam vitae dolore placeat ipsum
+      saepe ipsam ratione nulla. Laborum ratione quis odit exercitationem et ex
+      voluptatum expedita vero magni assumenda. Quidem rem quia ex ducimus
+      delectus ipsum soluta eveniet quae veritatis? Vero iusto totam facere,
+      autem molestiae facilis adipisci, illum voluptatibus nulla officiis
+      architecto qui repellat!
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis deleniti
+      consequuntur maiores exercitationem officia dolor ipsa consequatur
+      doloribus amet veniam beatae in, repudiandae maxime, saepe, dolores dicta
+      pariatur earum molestias perspiciatis reprehenderit quae obcaecati.
+      Mollitia laudantium illum id dicta beatae. Reprehenderit expedita odio
+      nemo tempore quibusdam architecto, corrupti suscipit dolor ipsam, non,
+      velit corporis impedit asperiores quas! Reiciendis temporibus voluptas
+      quidem ducimus eveniet aliquid modi nihil ipsa alias accusamus vero sed ad
+      laudantium odio, nobis est aliquam quos. Sint earum illum consectetur
+      laudantium quo, eos hic voluptatum vero alias molestiae corporis modi et
+      nam quae impedit maxime, fugiat ex iste!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis deleniti
+      consequuntur maiores exercitationem officia dolor ipsa consequatur
+      doloribus amet veniam beatae in, repudiandae maxime, saepe, dolores dicta
+      pariatur earum molestias perspiciatis reprehenderit quae obcaecati.
+      Mollitia laudantium illum id dicta beatae. Reprehenderit expedita odio
+      nemo tempore quibusdam architecto, corrupti suscipit dolor ipsam, non,
+      velit corporis impedit asperiores quas! Reiciendis temporibus voluptas
+      quidem ducimus eveniet aliquid modi nihil ipsa alias accusamus vero sed ad
+      laudantium odio, nobis est aliquam quos. Sint earum illum consectetur
+      laudantium quo, eos hic voluptatum vero alias molestiae corporis modi et
+      nam quae impedit maxime, fugiat ex iste!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
